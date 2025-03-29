461b64c8a692d16846c6c329cb50d90f74132a72
mrtanuarta <rickytanuarta123@gmail.com>
2025-03-29 17:19:09 +0800

    Simple Animation
    Can toy around with it more if you want

--- Shallow Depths/Assets/Scripts/PlayerMovement.cs
+++ Shallow Depths/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
+    
     private Vector2 _movement;
+    
     private Rigidbody2D _rb;
+    private Animator  _animator;
+
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +25,8 @@ public class PlayerMovement : MonoBehaviour
     {
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
         _rb.linearVelocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
     }
 }
