bc2f26441cae4f57deed09af1357b2e5d59025e4
Estody <aquneagames@gmail.com>
2025-03-31 15:32:31 +0700

    Interact E

--- Shallow Depths/.github/copilot-instructions.md
+++ Shallow Depths/.github/copilot-instructions.md
@@ -1,8 +1,14 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Shallow Depths
 - Unity version: Unity 6000.0.42f1
+- Active scene:
+  - Name: SampleScene
+  - Tags:
+    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, NPC
+  - Layers:
+    - Default, TransparentFX, Ignore Raycast, Water, UI
 - Active game object:
-  - Name: DialogueManager
+  - Name: Player
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Shallow Depths/Assets/Scripts/NPCDialogue.cs
+++ Shallow Depths/Assets/Scripts/NPCDialogue.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NPCDialogue : MonoBehaviour
 {
     public TextDialogue[] dialogues; // Array of multiple dialogues
-
+    [SerializeField] private GameObject _InteractUI;
+    private Collider2D Collider2D;
+    private GameObject _currentInteractUI;
+    void Start()
+    {
+        Collider2D = GetComponent<Collider2D>();
+    }
     public void TriggerDialogue()
     {
         DialogueManager.Instance.StartDialogue(dialogues);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _currentInteractUI = Instantiate(_InteractUI);
+        
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(_currentInteractUI);
     }
 }
